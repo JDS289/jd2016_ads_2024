ea5ef2f0f0af6ece5793fdf21351be1b1fcb1bc7
JDS289 <132592001+JDS289@users.noreply.github.com>
2024-11-26 02:18:06 +0000

    Update access.py

--- fynesse/access.py
+++ fynesse/access.py
@@ -5,6 +5,7 @@ import csv
 import time
 import osmnx as ox
 from math import cos, radians
+import os
 import warnings
 
 """These are the types of import we might expect in this file
@@ -17,31 +18,6 @@ import sqlite"""
 # This file accesses the data
 
 """Place commands in this file to access the data electronically. Don't remove any missing values, or deal with outliers. Make sure you have legalities correct, both intellectual property and personal data privacy rights. Beyond the legal side also think about the ethical issues around this data. """
-
-def data():
-    """Read the data from the web or local file, returning structured format such as a data frame"""
-    raise NotImplementedError
-
-
-def hello_world():
-  print("Hello from the data science library!")
-
-
-
-def download_price_paid_data(year_from, year_to):
-    # Base URL where the dataset is stored 
-    base_url = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com"
-    """Download UK house price data for given year range"""
-    # File name with placeholders
-    file_name = "/pp-<year>-part<part>.csv"
-    for year in range(year_from, (year_to+1)):
-        print (f"Downloading data for year: {year}")
-        for part in range(1,3):
-            url = base_url + file_name.replace("<year>", str(year)).replace("<part>", str(part))
-            response = requests.get(url)
-            if response.status_code == 200:
-                with open("." + file_name.replace("<year>", str(year)).replace("<part>", str(part)), "wb") as file:
-                    file.write(response.content)
 
 
 def create_connection(user, password, host, database, port=3306):
@@ -67,6 +43,7 @@ def create_connection(user, password, host, database, port=3306):
     except Exception as e:
         print(f"Error connecting to the MariaDB Server: {e}")
     return conn
+
 
 def make_box(centre_lat, centre_lon, side_length): # side_length in km; returns lat_high, lat_low, lon_high, lon_low
   # note that we additionally divide by two (hence using 222 not 111) because side_length is 2*(distance from centre to side)
@@ -102,6 +79,38 @@ def count_pois_near_coordinates(latitude: float, longitude: float, tags: dict, d
 
 
 
+def download_csv(url):
+  """Downloads a CSV file from the given URL and returns the path to the downloaded file."""
+  counter = 1
+  while os.path.exists(f"file{counter}.csv"):
+    counter += 1
+  csv_path = f"file{counter}.csv"
+
+  with open(csv_path, "wb") as file:
+    file.write(requests.get(url).content)
+
+  return file.name
+
+
+
+
+"""Below this point are functions which were given in practicals; aside from the specific practical for each, I will avoid using them."""
+def download_price_paid_data(year_from, year_to):
+    # Base URL where the dataset is stored 
+    base_url = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com"
+    """Download UK house price data for given year range"""
+    # File name with placeholders
+    file_name = "/pp-<year>-part<part>.csv"
+    for year in range(year_from, (year_to+1)):
+        print (f"Downloading data for year: {year}")
+        for part in range(1,3):
+            url = base_url + file_name.replace("<year>", str(year)).replace("<part>", str(part))
+            response = requests.get(url)
+            if response.status_code == 200:
+                with open("." + file_name.replace("<year>", str(year)).replace("<part>", str(part)), "wb") as file:
+                    file.write(response.content)
+
+
 def housing_upload_join_data(conn, year):
   start_date = str(year) + "-01-01"
   end_date = str(year) + "-12-31"
@@ -122,3 +131,12 @@ def housing_upload_join_data(conn, year):
   cur.execute(f"LOAD DATA LOCAL INFILE '" + csv_file_path + "' INTO TABLE `prices_coordinates_data` FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED by '\"' LINES STARTING BY '' TERMINATED BY '\n';")
   conn.commit()
   print('Data stored for year: ' + str(year))
+
+
+def data():
+    """Read the data from the web or local file, returning structured format such as a data frame"""
+    raise NotImplementedError
+
+
+def hello_world():
+  print("Hello from the data science library!")
