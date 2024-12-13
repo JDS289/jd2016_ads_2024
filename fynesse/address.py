e77cc3f868625c64b7bb3e77f2a4ee792485e821
JDS289 <132592001+JDS289@users.noreply.github.com>
2024-12-13 07:53:43 +0000

    Update address.py

--- fynesse/address.py
+++ fynesse/address.py
@@ -17,3 +17,20 @@ import scipy.stats"""
 """Address a particular question that arises from the data"""
 
 
+
+from scipy.stats import pearsonr
+from sklearn.linear_model import LinearRegression
+
+
+def scatter(ax, predictions, actual, xlabel="", ylabel=""):
+  """`predictions` and `actual` should be DataFrames"""
+  
+  ax.scatter(predictions, actual)
+
+  tester_model = LinearRegression()
+  tester_fitted = tester_model.fit(predictions.to_numpy().reshape(-1, 1), actual.to_numpy())
+  m, c2 = tester_fitted.coef_[0], tester_fitted.intercept_
+  ax.plot([0, (max_f:=max(predictions))], [c2, m*max_f + c2], color="red")
+  ax.set_xlabel(xlabel)
+  ax.set_ylabel(ylabel)
+  ax.set_title(f"Correlation: {pearsonr(predictions, actual)[0]}", fontsize=10)
