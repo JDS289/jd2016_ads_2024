eb0c13ffd6cccd11dddbdb04feac17eb25fdc1cd
JDS289 <132592001+JDS289@users.noreply.github.com>
2024-12-13 06:55:40 +0000

    Update assess.py

--- fynesse/assess.py
+++ fynesse/assess.py
@@ -8,42 +8,26 @@ import numpy as np
 from math import cos, radians
 import osmnx as ox
 import pandas as pd
+import geopandas as gpd
 import matplotlib.pyplot as plt
 from scipy.stats import pearsonr
 from sklearn.linear_model import LinearRegression
 import pymysql
-
-"""These are the types of import we might expect in this file
-import pandas
-import bokeh
-import seaborn
-import matplotlib.pyplot as plt
-import sklearn.decomposition as decomposition
-import sklearn.feature_extraction"""
+import shapely
 
 """Place commands in this file to assess the data you have downloaded. How are missing values encoded, how are outliers encoded? What do columns represent, makes rure they are correctly labeled. How is the data indexed. Crete visualisation routines to assess the data (e.g. in bokeh). Ensure that date formats are correct and correctly timezoned."""
 
 
-def data():
-    """Load the data from access and ensure missing values are correctly encoded as well as indices correct, column names informative, date and times correctly formatted. Return a structured data structure such as a data frame."""
-    df = access.data()
-    raise NotImplementedError
-
-def query(data):
-    """Request user input for some aspect of the data."""
-    raise NotImplementedError
-
-def view(data):
-    """Provide a view of the data that allows the user to verify some aspect of its quality."""
-    raise NotImplementedError
-
-def labelled(data):
-    """Provide a labelled set of data ready for supervised learning."""
-    raise NotImplementedError
+def resultsToGDF(results, geomColumnName="geom"):
+  # from the results of an SQL query; and transforms to UK metres coordinates
+  df = gpd.GeoDataFrame(results)
+  geom = df.get(geomColumnName).apply(lambda geomString: shapely.from_wkt(geomString))
+  df = df.drop(columns=[geomColumnName])
+  return gpd.GeoDataFrame(df, geometry=geom).set_crs("EPSG:4326").to_crs(crs="EPSG:27700")
 
 
 def scatter(ax, predictions, actual, xlabel="", ylabel=""):
-    """`predictions` and `actual` should be DataFrames for now, please"""
+    """`predictions` and `actual` should be DataFrames"""
     
     ax.scatter(predictions, actual)
 
