d6468a199c6b55d3c84d5b2ad679316fb25948d8
JDS289 <132592001+JDS289@users.noreply.github.com>
2024-12-20 08:13:21 +0000

    Update address.py

--- fynesse/address.py
+++ fynesse/address.py
@@ -26,14 +26,19 @@ def greenProportion_join_priceStDev(conn, year):
   return priceStDevDF.join(greenGDF, how="inner").astype({"green_proportion":float, "price_stdev":float})
 
 
-def GLM_predict(frame, fit_intercept=True):
+def GLM_predict(frame, fit_intercept=True, print_coefs=False):
   """Returns the predictions of a created (Generalised) Linear Model for a given DataFrame,
      whose final column should contain the target variable."""
   fitted = LinearRegression(fit_intercept=fit_intercept).fit(frame.iloc[:,:-1].to_numpy(),
                                                              frame.iloc[:,-1].to_numpy().reshape(-1, 1))
-  predictions = fitted.intercept_[0] if fit_intercept else np.zeros(len(frame.index))
+  intercept = fitted.intercept_[0] if fit_intercept else 0
+  if print_coefs:
+    print(*(list(fitted.coef_[0])+[intercept]))
+  predictions = intercept * np.ones(len(frame.index))
+
   for explanatory_i in range(len(frame.columns)-1):
     predictions += fitted.coef_[0][explanatory_i] * frame.iloc[:, explanatory_i]
+  
 
   return predictions 
 
